a8f395d741b4bad7cfe843ea9ad690ab5a9159d6
baripembo <erikawei@gmail.com>
2017-01-09 21:17:01 -0500

    Check for last position in timeline events

--- src/js/index.js
+++ src/js/index.js
@@ -82,6 +82,7 @@ $(document).ready(function(){
     $.each( data.timeline.years, function( key, val ) {
       $('.timeline-chart').append('<div class="date-flag">'+val.year+'</div>');
       var now = new Date();
+      var isCurrent = (val.year == now.getFullYear()) ? true : false;
       var events = val.events;
       for (var i=0; i < events.length; i++){
         var eventDate = new Date(events[i].date);
